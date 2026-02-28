186e6715cdbe4c3ab8a07c314bba37e3712e81d3
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-02-28 17:32:16 +0530

    Remove comments

--- mobile/app/(main)/chats.tsx
+++ mobile/app/(main)/chats.tsx
@@ -52,7 +52,6 @@ const Chats = () => {
     socket.emit("join", user?._id);
 
     socket.on("receiveMessage", () => {
-      // When message received → refresh conversations
       fetchConversations();
     });
 
